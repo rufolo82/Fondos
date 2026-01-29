3394012917704289382f1b60b3e47a3582e6c88f
rufolo82 <rlm1982@gmail.com>
2026-01-29 22:34:22 +0100

    Update aggregator.py

--- data_sources/aggregator.py
+++ data_sources/aggregator.py
@@ -1,9 +1,13 @@
-from data_sources.quefondos import get_from_quefondos
-from data_sources.finect import get_from_finect
 from data_sources.investing import get_from_investing
+from data_sources.finect import get_from_finect
+from data_sources.quefondos import get_from_quefondos
 
 def get_fund_data(isin):
-    for source in (get_from_quefondos, get_from_finect, get_from_investing):
+    for source in (
+        get_from_investing,   # 👈 principal
+        get_from_finect,
+        get_from_quefondos,
+    ):
         data = source(isin)
         if data:
             return data
